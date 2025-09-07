eb42d926862fd4a0eb069cdad737faa126e5cecd
bardia <bardia.azizian@gmail.com>
2025-09-06 18:28:41 -0700

    add: jpeg compression, bpp calculation, result saving

--- internvl_chat/eval/mme/eval.py
+++ internvl_chat/eval/mme/eval.py
@@ -3,6 +3,7 @@ import os
 import re
 from pathlib import Path
 import cv2
+import numpy as np
 
 import torch
 from internvl.model import load_model_and_tokenizer
@@ -12,13 +13,14 @@ from tqdm import tqdm
 
 from sfu_torch_lib import io
 
-from multimodal_coding.utils.utils import compress_image_jpeg
+from multimodal_coding.utils.utils import compress_image_jpeg_pil, compress_image_jpeg_cv, update_json_file
 from multimodal_coding.prefilter import tinyclip_filter
 from multimodal_coding.utils.utils import is_debug_mode
 
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[4]
+MME_ROOT = FILE.parents[0]
 
 
 def load_image(
@@ -34,6 +36,7 @@ def load_image(
     width, height = orig_image.size
     num_pixels = width * height
     orig_bpp = (file_size_bytes * 8) / num_pixels
+    compressed_bpp = 0
 
     if args.prefilter:
         image_filtered = tinyclip_filter(
@@ -49,39 +52,39 @@ def load_image(
             ksize=args.ksize
         )
         # Encode (compress) to JPEG in memory
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 75]  # quality from 0–100
-        success, encoded_img = cv2.imencode('.jpg', image_filtered, encode_param)
-        assert success, "Image encoding failed"
-        compressed_bpp = encoded_img.size * 8 / num_pixels
-        # Decode (load back into cv2 NumPy array)
-        decoded_img = cv2.imdecode(encoded_img, cv2.IMREAD_COLOR)
-        # Convert to PIL
-        cv_img_rgb = cv2.cvtColor(decoded_img, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(cv_img_rgb)
+        image, compressed_bpp = compress_image_jpeg_cv(image_filtered, quality=args.compress_ratio)
         if args.verbose:
             image.show()
     else:
         image = orig_image
-
-    if args.compress_ratio != 0:
-        image = compress_image_jpeg(image, quality=args.compress_ratio)
+        if args.compress_ratio != 0:
+            # image, bpp = compress_image_jpeg_pil(image, quality=args.compress_ratio)
+            cv_img = np.array(image)
+            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
+            image, compressed_bpp = compress_image_jpeg_cv(cv_img, quality=args.compress_ratio)
+            if args.verbose:
+                image.show()
 
     transform = build_transform(is_train=False, input_size=input_size)
     if args.dynamic:
         images = dynamic_preprocess(image, image_size=input_size,
                                     use_thumbnail=use_thumbnail,
                                     max_num=args.max_num)
-        orig_images = dynamic_preprocess(orig_image, image_size=input_size,
-                                         use_thumbnail=use_thumbnail,
-                                         max_num=args.max_num)
+        if args.verbose:
+            orig_images = dynamic_preprocess(orig_image, image_size=input_size,
+                                            use_thumbnail=use_thumbnail,
+                                            max_num=args.max_num)
     else:
         images = [image]
-        orig_images = [orig_image]
+        orig_images = [orig_image] if args.verbose else None
     pixel_values = [transform(image) for image in images]
     pixel_values = torch.stack(pixel_values)
-    orig_pixel_values = [transform(image) for image in orig_images]
-    orig_pixel_values = torch.stack(orig_pixel_values)
-    return pixel_values, orig_pixel_values
+    if args.verbose:
+        orig_pixel_values = [transform(image) for image in orig_images]
+        orig_pixel_values = torch.stack(orig_pixel_values)
+    else:
+        orig_pixel_values = None
+    return pixel_values, orig_pixel_values, orig_bpp, compressed_bpp
 
 
 def post_processing(response):
@@ -98,6 +101,7 @@ if __name__ == '__main__':
             str(io.localize_cached(path="s3://models/InternVL/InternVL2_5-1B.zip")))
     parser.add_argument('--data-path', type=str, default=str((ROOT / 'InternVL/internvl_chat/data/mme/MME_Benchmark_release_version').resolve()) if is_debug_mode() else \
             str(io.localize_dataset(path="s3://datasets/multimodal", overwrite=False)))
+    parser.add_argument('--json-result-path', type=str, default=str(MME_ROOT / 'results.json'))
     parser.add_argument('--root', type=str, default=str(ROOT / 'InternVL/internvl_chat/eval/mme/Your_Results'))
     parser.add_argument('--num-beams', type=int, default=1)
     parser.add_argument('--top-k', type=int, default=50)
@@ -136,7 +140,17 @@ if __name__ == '__main__':
     print(f'[test] use_thumbnail: {use_thumbnail}')
     print(f'[test] max_num: {args.max_num}')
 
-    output = os.path.basename(args.checkpoint)
+    model_id = (
+        f'{os.path.basename(args.checkpoint)}-'
+        f'compress_{args.compress_ratio}-'
+        f'method_{args.method}-'
+        f'prefilter_{int(args.prefilter)}-'
+        f'prescale_{args.prescale}-'
+        f'sigmamin_{args.sigma_min}-'
+        f'sigmamax_{args.sigma_max}-'
+        f'ksize_{args.ksize}'
+    )
+    output = MME_ROOT / model_id
     os.makedirs(output, exist_ok=True)
     prompt = 'Answer the question using a single word or phrase.'
 
@@ -145,14 +159,17 @@ if __name__ == '__main__':
         fout = open(os.path.join(output, filename), 'w', encoding='utf-8')
         lines = fin.readlines()
         filename = filename.replace('.txt', '')
+        orig_bpp_tot, compressed_bpp_tot = 0.0, 0.0
+        count = 0
         for line in tqdm(lines):
+            count += 1
             img, text, gt = line.strip().split('\t')
             question = text + ' ' + prompt
             img_path = os.path.join(args.data_path, filename, img)
             assert os.path.exists(img_path), img_path
-            pixel_values, orig_pixel_values = load_image(img_path, text, args, image_size)
-            pixel_values = pixel_values.cuda().to(torch.bfloat16)
-            orig_pixel_values = orig_pixel_values.cuda().to(torch.bfloat16)
+            pixel_values, orig_pixel_values, orig_bpp, compressed_bpp = load_image(img_path, text, args, image_size)
+            orig_bpp_tot += orig_bpp
+            compressed_bpp_tot += compressed_bpp
             generation_config = dict(
                 do_sample=args.sample,
                 num_beams=args.num_beams,
@@ -164,24 +181,57 @@ if __name__ == '__main__':
                 generation_config["top_p"] = args.top_p
             response = model.chat(
                 tokenizer=tokenizer,
-                pixel_values=pixel_values,
-                question=question,
-                generation_config=generation_config,
-                verbose=False
-            )
-            orig_response = model.chat(
-                tokenizer=tokenizer,
-                pixel_values=orig_pixel_values,
+                pixel_values=pixel_values.cuda().to(torch.bfloat16),
                 question=question,
                 generation_config=generation_config,
                 verbose=False
             )
             response = post_processing(response)
-            orig_response = post_processing(orig_response)
+            if args.verbose:
+                orig_response = model.chat(
+                    tokenizer=tokenizer,
+                    pixel_values=orig_pixel_values.cuda().to(torch.bfloat16),
+                    question=question,
+                    generation_config=generation_config,
+                    verbose=False
+                )
+                orig_response = post_processing(orig_response)
             print(img, question, gt, response, sep='\t', file=fout)
             if args.verbose:
                 print(text)
                 print("original response:", orig_response)
-                print("prefiltered image response:", response)
+                print("processed image response:", response)
         fin.close()
         fout.close()
+        orig_bpp_tot /= count
+        compressed_bpp_tot /= count
+        update_json_file(args.json_result_path, {
+            model_id: {
+                'hparams': {
+                    'top-k': args.top_k,
+                    'top-p': args.top_p,
+                    'sample': args.sample,
+                    'dynamic': args.dynamic,
+                    'max_num': args.max_num,
+                    'load-in-8bit': args.load_in_8bit,
+                    'load-in-4bit': args.load_in_4bit,
+                    'auto': args.auto,
+                    'prefilter': args.prefilter,
+                    'compress-ratio': args.compress_ratio,
+                    'sigma_min': args.sigma_min,
+                    'sigma_max': args.sigma_max,
+                    'method': args.method,
+                    'ksize': args.ksize,
+                    'prescale': args.prescale,
+                    'clip_arch': args.clip_arch,
+                    'clip_pretrained': args.clip_pretrained,
+                    'image_size': image_size,
+                    'use_thumbnail': use_thumbnail,
+                },
+                filename: {
+                    'orig_bpp': orig_bpp_tot,
+                    'compressed_bpp': compressed_bpp_tot,
+                    'count': count,
+                }
+            }
+        })
